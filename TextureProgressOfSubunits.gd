0fc4113069bdb4552af7374d2c259bac3d9447bb
James Lowrey <jarlowrey@gmail.com>
2018-05-18 16:06:34 -0700

    optimized changing the num_subunits, fixed subtle error with queue_free that caused incorrect percent values to occasionally be set

--- TextureProgressOfSubunits.gd
+++ TextureProgressOfSubunits.gd
@@ -11,34 +11,38 @@ func _ready():
 
 func set_percent(val):
 	percent = clamp(val,0,1)
+	
 	if is_inside_tree():
 		var val_per_subunit = 1.0 / num_subunits
 		var subunit_index = int(percent / val_per_subunit)
 		
-		var tps = $CreatedTextureProgresses.get_children()
-		for i in range( tps.size() ):
-			var tp = tps[i]
+		for i in range( num_subunits ):
+			var tp = $CreatedTextureProgresses.get_children()[i]
 			if i == subunit_index:
-				var remainder = (float(percent) / val_per_subunit - subunit_index)
+				var remainder = float(percent) / val_per_subunit - subunit_index
 				tp.value = remainder * tp.max_value
 			elif i < subunit_index:
 				tp.value = tp.max_value
 			else: 
 				tp.value = tp.min_value
 
-func set_num_subunits(val):
-	num_subunits = val
+func set_num_subunits(new_num_subunits):
+	new_num_subunits = ceil(new_num_subunits)
+	
 	if is_inside_tree():
-		for child in $CreatedTextureProgresses.get_children():
-			child.queue_free()
-		
-		for i in range(num_subunits):
-			var tp = $ReferenceTextureProgress.duplicate()
-			$CreatedTextureProgresses.add_child(tp)
-			tp.rect_position = Vector2(i * (tp.rect_size.x + margin_x), 0)
-			tp.visible = true
-		
-		set_percent(percent)
+		if new_num_subunits != $CreatedTextureProgresses.get_children().size(): #do not redo if nothing has changed
+			for child in $CreatedTextureProgresses.get_children():
+				$CreatedTextureProgresses.remove_child(child) #queue free does not remove immediately, but for other things to work it needs to be done immediately
+				child.queue_free()
+			
+			for i in range(new_num_subunits):
+				var tp = $ReferenceTextureProgress.duplicate()
+				$CreatedTextureProgresses.add_child(tp)
+				tp.rect_position = Vector2(i * (tp.rect_size.x + margin_x), 0)
+				tp.visible = true
+	
+	num_subunits = new_num_subunits
+	set_percent(percent)
 
 func get_width():
 	var tp = _last_tp()
